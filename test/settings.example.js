92c9b18b8ef76ea336aa81142829b461bbeff8fc
James Johnson <johnsojr@gmail.com>
2017-04-09 00:33:51 -0500

    feat(destroy): added destroy() method to end client connections to server
    CLoses #5 ... I think. :)

--- test/settings.example.js
+++ test/settings.example.js
@@ -1,10 +1,8 @@
 export default {
-  ldap: {
-    url: 'ldap://0.0.0.0:1389',
-    base: 'dc=users,dc=localhost',
-    bind: {
-      dn: 'cn=root',
-      password: 'secret',
-    },
+  url: 'ldap://0.0.0.0:1389',
+  base: 'dc=users,dc=localhost',
+  bind: {
+    dn: 'cn=root',
+    password: 'secret',
   },
 };
