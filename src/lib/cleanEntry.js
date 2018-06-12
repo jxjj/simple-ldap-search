6d11afadf61396772f1c5439f839cd4dfcb648b0
James Johnson <james_johnson@mcad.edu>
2018-06-12 16:59:13 -0500

    resolve audit warnings
    Updated deps and removed some unnecessary deps
    (nsp, npm-check-audits, lodash)

--- src/lib/cleanEntry.js
+++ src/lib/cleanEntry.js
@@ -1,18 +1,25 @@
-import lodash from 'lodash';
-
 export default function cleanEntry(entryObj) {
   if (entryObj instanceof Array) return entryObj.map(cleanEntry);
-  return lodash.chain(entryObj)
-    .omit('controls')
-    .mapValues((value) => {
-      // cleanup booleans
-      if (value === 'TRUE') { return true; }
-      if (value === 'FALSE') { return false; }
 
-      // if integer string, convert to number
-      if (/^(-|\+)?\d+$/.test(value)) { return parseInt(value, 10); }
+  // omit controls
+  const { controls, ...rest } = entryObj;
 
-      // otherwise the regular value
-      return value;
-    }).value();
+  return Object.keys(rest).reduce((acc, key) => {
+    const value = rest[key];
+    // cleanup booleans
+    if (value === 'TRUE') {
+      return { ...acc, [key]: true };
+    }
+    if (value === 'FALSE') {
+      return { ...acc, [key]: false };
+    }
+
+    // if integer string, convert to number
+    if (/^(-|\+)?\d+$/.test(value)) {
+      return { ...acc, [key]: parseInt(value, 10) };
+    }
+
+    // otherwise the regular value
+    return { ...acc, [key]: value };
+  }, {});
 }
